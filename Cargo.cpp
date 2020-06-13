4a66fd0a09978086097b145f1db4dba23dbcc593
Daniel Paul Grech Pereira <13129650+pereiradaniel@users.noreply.github.com>
2020-06-13 16:35:41 -0400

    Add cargo.cpp and member functions/prototypes.

---
+++ Cargo.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Cargo.h"
+
+using namespace std;
+
+namespace sdds
+{
+	void initialize(const char* desc, double weight)
+	{
+	}
+
+	
+	const char* getDesc() const
+	{
+	};
+
+
+	double getWeight() const
+	{	
+	};
+
+	
+	void setDesc(const char* description)
+	{
+	};
+
+
+	void setWeight(double weight)
+	{
+	};
+
+}
--- Cargo.h
+++ Cargo.h
@@ -12,11 +12,19 @@
 namespace sdds
 {
 	const int MAX_DESC = 20;
+	const double MAX_WEIGHT = 777.555;
+	const double MIN_WEIGHT = 44.2222;
 
-	struct Cargo
+	class Cargo
 	{
 		char cargo_description[MAX_DESC + 1];  // +1 for null terminator.
 		double cargo_weight;
+	private:
+		void initialize(const char* desc, double weight);
+		const char* getDesc() const;
+		double getWeight() const;
+		void setDesc(const char* description);
+		void setWeight(double weight);
 	};
 }
 
